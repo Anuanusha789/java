31c9c2e9495f179adfef8986fdecd742380ef188
beerannagari Anusha <beerannagarianusha@gmail.com>
2023-03-06 14:51:38 +0530

    day6 commit

---
+++ day6/Trianglechar.java
@@ -0,0 +1,20 @@
+class Trianglechar
+{
+    public static void main(String args[])
+    {
+        int n=8;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print(" ");
+            }
+            
+
+        
+        char start='A';
+        for(int k=1;k<=i;k++)
+        {
+            System.out.print(start++ +" ");
+        }  System.out.println();  }
+}}
