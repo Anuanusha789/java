220929c446f25a99e90d944bb2eeb186a8e5fe43
beerannagari Anusha <beerannagarianusha@gmail.com>
2023-03-15 10:51:00 +0530

    Commit

---
+++ Day-16/SpaceComplexity1.java
@@ -0,0 +1,8 @@
+class SpaceComplexity1
+{
+    public static void main(String args[])
+    {
+        int n=1000;
+        System.out.print(n);
+    }
+}
---
+++ Day-16/SpaceComplexity_n.java
@@ -0,0 +1,10 @@
+class SpaceComplexity_n
+{
+    public static void main(String args[])
+    {
+        int n=1000;
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        System.out.println(i);
+    }
+}
